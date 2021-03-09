a75479b73b2ff95f54d1d9ad584473a4cea967c0
stephen <stephensmagina@gmail.com>
2021-03-09 10:09:09 +0300

    Initial commit

---
+++ app/src/main/java/com/example/recyclerviewautoslide/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.recyclerviewautoslide;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import net.seifhadjhassen.recyclerviewpager.PagerAdapter;
+import net.seifhadjhassen.recyclerviewpager.PagerModel;
+import net.seifhadjhassen.recyclerviewpager.RecyclerViewPager;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    RecyclerViewPager recyclerViewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RecyclerViewPager recyclerViewPager=findViewById(R.id.pager);
+        // add item from resource
+
+        recyclerViewPager.addItem(new PagerModel(R.drawable.cover1,"Vikings",getApplicationContext()));
+        recyclerViewPager.addItem(new PagerModel(R.drawable.cover2,"The nutcracker and the four realms",getApplicationContext()));
+        recyclerViewPager.addItem(new PagerModel(R.drawable.cover3,"Flash",getApplicationContext()));
+
+        //add item from url
+
+        //  recyclerViewPager.addItem(new PagerModel("http://i.imgur.com/DvpvklR.png","Vikings"));
+        recyclerViewPager.start();
+
+        recyclerViewPager.setOnItemClickListener(new PagerAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(int position) {
+                Toast.makeText(getApplicationContext(),"click pos "+position,Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
